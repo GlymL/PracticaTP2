d9ef56247ae0f526fe64219a54e7bf1b6e9383e4
ruben <ruben@caonero>
2025-01-31 13:34:07 +0100

    Junction updated

--- src/src/simulator/model/Junction.java
+++ src/src/simulator/model/Junction.java
@@ -12,8 +12,8 @@ public class Junction extends SimulatedObject{
 	private List<Road> _listRoad;
 	private Map<Junction, Road> _mapRoads;
 	private List<List<Vehicle>> _queueList;
-	private int _index;
-	private int _lastIndex;
+	private int _currentGreenTraffic;
+	private int _lastGreenTraffic;
 	private LightSwitchingStrategy _lsStrategy;
 	private DequeuingStrategy _dqStrategy;
 	private int _xCoor;
@@ -28,12 +28,18 @@ public class Junction extends SimulatedObject{
 		  _dqStrategy = dqStrategy;
 		  _xCoor = xCoor;
 		  _yCoor = yCoor;
+		  _currentGreenTraffic = -1;
+		  _lastGreenTraffic = 0;
 		}
 
 	@Override
 	void advance(int time) {
 		// TODO Auto-generated method stub
-		
+		List<Vehicle> removable = _dqStrategy.dequeue(_queueList.get(_currentGreenTraffic)); //llama a la funcion con la cola del semaforo actual
+		for(int i= 0; i < removable.size(); i++) {
+			removable.get(i).moveToNextRoad();
+			
+		}
 	}
 
 	@Override
@@ -49,19 +55,23 @@ public class Junction extends SimulatedObject{
 		_queueList.add(lista);
 	}
 	void addOutGoingRoad(Road r) { // TODO not finished
-		Junction j;
-		for(int i = 0; i < this._listRoad.size(); i++) {
+		if( r.get_srcJunc() != this) //carretera no es de este cruce
+			 throw new IllegalArgumentException("Invalid type/desc");
+		for(int i = 0; i < this._listRoad.size(); i++) { //ya existe una carretera con mismo inicio y final
 			if(this._listRoad.get(i).get_srcJunc() == r.get_srcJunc() && this._listRoad.get(i).get_destJunc() == r.get_destJunc())
-				return;
+				throw new IllegalArgumentException("Invalid type/desc");
 		}
+		Junction j;
 		j = r.get_destJunc();
 		_mapRoads.put(j, r);
 	}
 	void enter(Vehicle v) {
 		Road r = v.get_road();
-		
-		
-		
+		int aux = _listRoad.indexOf(r); //veo la pos de la carretera
+		_queueList.get(aux).add(v); //como estan en el mismo orden, añado el coche a la cola de la carretera
+	}
+	Road roadTo(Junction j) {
+		return _mapRoads.get(j);
 	}
 	
 	
